9c8935aa65f8d5432fa6b0828554dbefc64917fb
MonsieurSingh <teghjyot@icloud.com>
2025-09-11 00:46:19 +0200

    Initial Commit

---
+++ cpp_solutions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_solutions
+//
+//  Created by Teghjyot Singh on 11/9/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
